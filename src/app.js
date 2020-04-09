ac12e45089a853f4064323074404a4b3f06eb5bc
Leandro Souza <leandror.dev@gmail.com>
2020-04-09 10:26:31 -0300

    List repositories

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const {} = require()
 
-// const { uuid } = require("uuidv4");
+
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,15 +12,20 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get("/repositories", (req, res) => {
+  if (repositories[0] == null) {
+    return res.status(404).json({error:'Respositories not found'});
+  }
+
+  return res.json(repositories)
+
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post("/repositories", (req, res) => {
+
 });
 
-app.put("/repositories/:id", (request, response) => {
+app.put("/repositories/:id", (req, res) => {
   // TODO
 });
 
@@ -26,7 +33,7 @@ app.delete("/repositories/:id", (req, res) => {
   // TODO
 });
 
-app.post("/repositories/:id/like", (request, response) => {
+app.post("/repositories/:id/like", (req, res) => {
   // TODO
 });
 
